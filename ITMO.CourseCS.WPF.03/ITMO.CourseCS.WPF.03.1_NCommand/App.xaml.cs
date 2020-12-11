0de654f69a27250306ae9626c1cc36f768d54253
Евгения Сокольская <73002756+SokolskayaEvgenia@users.noreply.github.com>
2020-12-12 01:34:34 +0300

    практическая работа 3

---
+++ ITMO.CourseCS.WPF.03/ITMO.CourseCS.WPF.03.1_NCommand/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITMO.CourseCS.WPF._03._1_NCommand
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ITMO.CourseCS.WPF.03/ITMO.CourseCS.WPF.03.1_NCommand/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ITMO.CourseCS.WPF._03._1_NCommand
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CommandBinding abinding = new CommandBinding();
+            abinding.Command = CustomCommands.Launch;
+            abinding.Executed += new ExecutedRoutedEventHandler(Launch_Handler);
+            abinding.CanExecute += new CanExecuteRoutedEventHandler(LaunchEnabled_Handler);
+            this.CommandBindings.Add(abinding);
+        }
+        private void Launch_Handler(object sender, ExecutedRoutedEventArgs e)
+        {
+            richTextBox.AppendText(textBox.Text);
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("log.txt", true))
+            {
+                writer.WriteLine("Внесено {0}: {1} ", textBox.Text, DateTime.Now.ToShortDateString() + ", время: " +
+                DateTime.Now.ToLongTimeString());
+                writer.Flush();
+            }
+        }
+        private void LaunchEnabled_Handler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (bool)checkBox.IsChecked;
+        }
+    }
+
+}
